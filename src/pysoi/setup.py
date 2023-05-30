caa38e8541978a1230e35c7b8d2438d3cfdc93b0
Cédric Donner <cedonner@gmail.com>
2023-05-30 22:33:29 +0000

    sauvegarde automatique

--- src/pysoi/setup.py
+++ src/pysoi/setup.py
@@ -3,29 +3,10 @@ setup(
     name='pysoi',
     version='1.0',
     author='Cédric Donner',
+    author_email='cedonner@gmail.com',
     description='Tool to facilitate debugging SOI tasks in gitpod',
     long_description='Tool to facilitate debugging SOI tasks in gitpod',
-    url='',
     keywords='competitive programming',
     python_requires='>=3.7, <4',
-    packages=find_packages(include=['exampleproject', 'exampleproject.*']),
-    install_requires=[
-    ],
-    package_data={
-        
-    },
-    entry_points={
-    }
+    packages=['pysoi'],
 )
-from distutils.core import setup
-
-setup(
-    name='pysoi',
-    version='1.0',
-    author='Cédric Donner',
-    description='Tool to facilitate debugging SOI tasks in gitpod',
-    long_description='Tool to facilitate debugging SOI tasks in gitpod',
-    url='',
-      author_email='gward@python.net',
-      packages=['distutils', 'distutils.command'],
-     )
