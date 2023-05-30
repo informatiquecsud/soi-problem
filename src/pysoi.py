0643790823a536fcc3cdc6781ecaca8ecd2f84c2
Cédric Donner <cedonner@gmail.com>
2023-05-30 22:44:22 +0000

    simplifying without package pysoi

--- src/pysoi.py
+++ src/pysoi.py
@@ -4,8 +4,7 @@ import os
 TEST_EXTENSION = ".in"
 
 
-def load_test(filename, folder, testdir="tests", extension=TEST_EXTENSION, debug=False):
-    os.chdir(folder)
+def load_test(filename, testdir="tests", extension=TEST_EXTENSION, debug=False):
     # Redirecting the selected file to standard input for debugging purposes
     sys.stdin = open(os.path.join(testdir, f"{filename}.in"), "r")
 
