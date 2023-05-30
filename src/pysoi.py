1004fc62ab534c689518f909f0bceeb2cfdc1408
Cédric Donner <cedonner@gmail.com>
2023-05-30 22:51:04 +0000

    sauvegarde automatique

--- src/pysoi.py
+++ src/pysoi.py
@@ -3,23 +3,6 @@ import os
 
 TEST_EXTENSION = ".in"
 
-
 def load_test(filename, testdir="tests", extension=TEST_EXTENSION, debug=False):
     # Redirecting the selected file to standard input for debugging purposes
     sys.stdin = open(os.path.join(testdir, f"{filename}.in"), "r")
-
-
-def load_soi_test1(filename, extension=TEST_EXTENSION):
-    file_path = os.path.dirname(__file__)
-    os.chdir(file_path)
-    test_dir = os.path.join("..", "tests")
-
-    files = os.listdir(path=test_dir)
-
-    for f in files:
-        f = os.path.join(test_dir, f)
-        if os.path.isfile(f) and f.endswith(extension):
-            name = f.split(extension)[0]
-            print(f"Found test {name}")
-
-    test_to_run = int(input("Test to run (just enter the test number): "))
