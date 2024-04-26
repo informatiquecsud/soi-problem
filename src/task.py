d6a34b440890a23123de493478c61d74c69af911
Cédric Donner <cedonner@gmail.com>
2024-04-26 06:57:45 +0000

    sauvegarde automatique

--- src/task.py
+++ src/task.py
@@ -1,5 +1,11 @@
 '''
 This file contains a bug to illustrate the debugger in the gitpod environment.
+
+TODO: to optimize, store for every affiche where the last geq has been seen ...
+which improves finding the last greater than
+
+other option : use some sort of dynamic programming to not only store the last
+geq, but also the last geq compared to a given value => eats a lot of RAM
 '''
 
 
@@ -12,16 +18,49 @@ except:
     pass
 
 def debug(*args, **kwargs):
-    level = kwargs.get('level', 0)
-    if level >= DBG_LEVEL:
+    level = kwargs.get('level', 1)
+    if level <= DBG_LEVEL:
         print(*args)
 
 DBG_LEVEL = 0
 
-def find_last_height_ge(heights, i, height):
-    while i >= 0:
-        if heights[i] >= height:
-            return i
+
+
+def find_last_height_geq(heights, last_geq, i):
+    '''
+    >>> last_geq = [0, 0, 0, 0, 0, 0, 0]
+    >>> heights = [4, 3, 2, 1, 3, 5, 3]
+    >>> find_last_height_geq(heights, last_geq, 0)
+    -1
+    >>> last_geq
+    [-1, 0, 0, 0, 0, 0, 0]
+    >>> find_last_height_geq(heights, 1)
+    0
+    >>> find_last_height_geq(heights, 3)
+    2
+    >>> find_last_height_geq(heights, 4)
+    1
+    >>> find_last_height_geq(heights, 5)
+    -1
+    >>> find_last_height_geq(heights, 6)
+    5
+    >>>
+    '''
+    height = heights[i]
+    i -= 1
+    
+    j = i
+    while j >= 0:
+        if heights[j] < height:
+            j = last_geq[j]
+        else:
+            return j
+    # j = -1
+    # for j in range(i, -1, -1):
+    #     if heights[j] >= height:
+    #         last_geq[i + 1] = j
+    #         return j
+    return -1
 
 def main():
     '''
@@ -33,22 +72,42 @@ def main():
     nb_queries = int(input())
     nb_visibles = [0 for i in range(nb_queries)]
     heights = [0 for i in range(nb_queries)]
+    last_geq = [0 for i in range(nb_queries)]
     
     index_affiche = 0
     for _ in range(nb_queries):
         query = [x for x in input().split(' ')]
         if len(query) == 1:
-            print("voir")
+            debug("query")
+            print(nb_visibles[index_affiche - 1])
         else:
             height = int(query[1])
             debug("coller", height)
             heights[index_affiche] = height
+            index_last_geq = find_last_height_geq(heights, last_geq, index_affiche)
+            debug("visibles", nb_visibles)
+            debug("heights", heights)
+            debug("last geq index", index_last_geq)
+            if index_last_geq == -1:
+                nb_visibles[index_affiche] = 1
+            elif heights[index_last_geq] == height:
+                nb_visibles[index_affiche] = nb_visibles[index_last_geq]
+            elif heights[index_last_geq] > height:
+                nb_visibles[index_affiche] = nb_visibles[index_last_geq] + 1
             
             index_affiche += 1
         
         
-        debug(index_affiche)
+        debug("index_affiche", index_affiche)
+        debug("-----------")
 
 
 if __name__ == '__main__':
     main()
+    
+    
+try:
+    import doctest
+    doctest.testmod()
+except:
+    print("Impossible de lancer les doctests")
